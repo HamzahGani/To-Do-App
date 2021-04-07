419e26f639e753794d90a06a64c78e7b701a676d
Hamzah Gani <h.gani@hotmail.com>
2021-04-07 12:37:27 +0200

    added functionality to checkbox through state

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,20 +1,59 @@
 import Greeting from "./Greeting"
 import TodoItem from "./TodoItem";
 import todoData from "../todoData"
+import React from "react";
 
 
-function MainContent() {
-    const todoComponents = todoData.map((item) => {
+class MainContent extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            todos:todoData
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        
+        this.setState((prevState) => {
+            const updatedTodos = prevState.todos.map((todo) =>{
+                if (todo.id === id) {
+                    alert(id);
+                    //todo.completed? todo.completed=false: todo.completed=true;
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                    //todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+        //alert("changed")
+    }
+
+    render(){
+        console.log(this.state.todos);
+
+        const todoComponents = this.state.todos.map((item) => {
+            return(
+                <TodoItem 
+                    key={item.id} 
+                    item={item} 
+                    handleChange={this.handleChange} 
+                />
+            )
+        })
         return(
-            <TodoItem key={item.id} list={item} />
-        )
-    })
-    return(
-        <div className="todo-list">
-            <Greeting />
-            {todoComponents}
-        </div>
-    );
+            <div className="todo-list">
+                <Greeting />
+                {todoComponents}
+            </div>
+        );
+    }
 }
 
 export default MainContent
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,15 +1,17 @@
 const TodoItem = (props) => {
     
-    return (
-        <div className="todo-item">
-            <input 
-                type="checkbox" 
-                checked={props.list.completed}
-                onChange={()=>{alert("changed")}}
-            />
-            <label>{props.list.listItem}</label><br/>
-        </div>
-    );
+    
+        return (
+            <div className="todo-item">
+                <input 
+                    type="checkbox" 
+                    checked={props.item.completed}
+                    onChange={() => {props.handleChange(props.item.id)}}
+                />
+                {props.item.completed? <label><b>{props.item.listItem}</b></label>: <label>{props.item.listItem}</label>}
+                <br/>
+            </div>
+        );    
 }
 
 export default TodoItem
